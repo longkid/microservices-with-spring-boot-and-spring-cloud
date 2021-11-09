7e26a53bf4dbf61294fdca54350bd43273bcda44
Lam Ho <hotienlam@gmail.com>
2021-11-09 21:56:40 +0700

    Step 09 - Implement POST Method to create a User

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserController.java
@@ -2,6 +2,8 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,5 +24,12 @@ public class UserController {
     @GetMapping("/users/{id}")
     public User retrieveOneUser(@PathVariable int id) {
         return userDaoService.findOne(id);
+    }
+
+    // input - details of user
+    // output - CREATED & return the created URI
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        User savedUser = userDaoService.save(user);
     }
 }
