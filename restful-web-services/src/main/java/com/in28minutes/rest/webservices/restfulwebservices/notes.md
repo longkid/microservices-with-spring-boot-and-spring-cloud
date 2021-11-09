5fd179a123e437f51171b0b0fa19300dbd052f33
Lam Ho <hotienlam@gmail.com>
2021-11-09 21:36:11 +0700

    Step 08 - Implement GET Methods for User Resource

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/notes.md
@@ -0,0 +1,14 @@
+# RESTful Web Services
+
+## Social Media Application Resource Mappings
+
+### User -> Posts
+
+- Retrieve all Users    - GET   /users
+- Create a User         - POST  /users
+- Retrieve one User     - GET   /users/{id} -> /users/1
+- Delete a User         - DELETE    /users/{id} -> /users/1
+
+- Retrieve all posts for a User     - GET /users/{id}/posts
+- Create a post for a User          - POST /users/{id}/posts
+- Retrieve details of a post        - GET /users/{id}/posts/{post_id}
---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserController.java
@@ -0,0 +1,26 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private final UserDaoService userDaoService;
+
+    public UserController(UserDaoService userDaoService) {
+        this.userDaoService = userDaoService;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveOneUser(@PathVariable int id) {
+        return userDaoService.findOne(id);
+    }
+}
