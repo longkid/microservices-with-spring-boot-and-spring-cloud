ae34f69ab32ca64c3dc943c145e5c0af440e3453
Lam Ho <hotienlam@gmail.com>
2021-11-22 21:56:12 +0700

    Step 18 - Internationalization for RESTful Services: get Locale by using LocaleContextHolder.getLocale()

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -2,12 +2,10 @@ package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
@@ -20,9 +18,13 @@ public class HelloWorldController {
         return "Hello World";
     }
 
+    /**
+     * Header: Accept-Language: en/fr
+     * @return "Good morning" message in different language
+     */
     @GetMapping(path = "/hello-world-internationalized")
-    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-        return messageSource.getMessage("good.morning.message", null, "Hello World", locale);
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage("good.morning.message", null, "Hello World", LocaleContextHolder.getLocale());
     }
 
     @GetMapping(path = "/hello-world-bean")
