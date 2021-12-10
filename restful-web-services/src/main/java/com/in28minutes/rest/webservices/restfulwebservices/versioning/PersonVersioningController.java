60cc67af3d1daf20694bd3baeb80e81e57df6025
Lam Ho <hotienlam@gmail.com>
2021-12-10 15:14:06 +0700

    Refactor: define common path (/person) for person resource

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -1,51 +1,53 @@
 package com.in28minutes.rest.webservices.restfulwebservices.versioning;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/person")
 public class PersonVersioningController {
 
     //---------------------- URI Versioning ----------------------
-    @GetMapping("/person/v1")
+    @GetMapping("/v1")
     public PersonV1 getPersonV1() {
         return new PersonV1("Lam Ho");
     }
 
-    @GetMapping("/person/v2")
+    @GetMapping("/v2")
     public PersonV2 getPersonV2() {
         return new PersonV2(new Name("Lam", "Ho"));
     }
 
     //---------------------- Request Parameter Versioning ----------------------
-    @GetMapping(value = "/person/param", params = "version=1")
+    @GetMapping(value = "/param", params = "version=1")
     public PersonV1 paramV1() {
         return new PersonV1("Lam Ho");
     }
 
-    @GetMapping(value = "/person/param", params = "version=2")
+    @GetMapping(value = "/param", params = "version=2")
     public PersonV2 paramV2() {
         return new PersonV2(new Name("Lam", "Ho"));
     }
 
     //---------------------- Header Versioning ----------------------
-    @GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
+    @GetMapping(value = "/header", headers = "X-API-VERSION=1")
     public PersonV1 headerV1() {
         return new PersonV1("Lam Ho");
     }
 
-    @GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
+    @GetMapping(value = "/header", headers = "X-API-VERSION=2")
     public PersonV2 headerV2() {
         return new PersonV2(new Name("Lam", "Ho"));
     }
 
     //---------------------- MIME type Versioning ----------------------
-    @GetMapping(value = "/person/produce", produces = "application/vnd.company.app-v1+json")
+    @GetMapping(value = "/produce", produces = "application/vnd.company.app-v1+json")
     public PersonV1 produceV1() {
         return new PersonV1("Lam Ho");
     }
 
-    @GetMapping(value = "/person/produce", produces = "application/vnd.company.app-v2+json")
+    @GetMapping(value = "/produce", produces = "application/vnd.company.app-v2+json")
     public PersonV2 produceV2() {
         return new PersonV2(new Name("Lam", "Ho"));
     }
