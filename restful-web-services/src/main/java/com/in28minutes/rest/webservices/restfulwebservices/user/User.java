b961bf39925975ab1716af87b0486d00840f9297
Lam Ho <hotienlam@gmail.com>
2022-01-20 23:02:58 +0700

    Step 33 - Create Post entity and define Many to One relationship with User entity

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,10 +1,14 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import com.in28minutes.rest.webservices.restfulwebservices.post.Post;
+
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -19,6 +23,9 @@ public class User {
 
     @Past
     private Date birthDate;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
 
     public User() {
     }
@@ -51,6 +58,14 @@ public class User {
 
     public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserRepository.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserRepository.java
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2022 Absolute Software Corporation. All rights reserved.
- * Reproduction or transmission in whole or in part, in any form, or by any means
- * (electronic, mechanical, or otherwise) is prohibited without the prior written
- * consent of the copyright owner.
- */
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
