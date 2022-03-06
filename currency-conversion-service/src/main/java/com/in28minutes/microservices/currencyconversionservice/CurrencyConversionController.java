2c64e046e5bfb152bc15a18c25988e141a05a0f6
Lam Ho <hotienlam@gmail.com>
2022-03-07 00:01:51 +0700

    Section 6 - Step 16: Create an API for currency conversion

---
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,19 @@
+package com.in28minutes.microservices.currencyconversionservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity) {
+        return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+    }
+}
