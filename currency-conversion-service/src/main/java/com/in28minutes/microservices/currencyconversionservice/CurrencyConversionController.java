3aebb5fe1a534c051026e19a914cae32a1d7f37f
Lam Ho <hotienlam@gmail.com>
2022-03-07 21:31:06 +0700

    Section 6 - Step 17: Invoke currency-exchange-service API from currency-conversion-service (using RestTemplate)

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -3,8 +3,11 @@ package com.in28minutes.microservices.currencyconversionservice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
@@ -14,6 +17,12 @@ public class CurrencyConversionController {
             @PathVariable String from,
             @PathVariable String to,
             @PathVariable BigDecimal quantity) {
-        return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        CurrencyConversion currencyConversion = new RestTemplate().getForObject("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+        return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
+                currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), "");
     }
 }
